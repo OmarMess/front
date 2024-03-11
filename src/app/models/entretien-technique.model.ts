4e1e1aff1d4cef91bbdeb7f85821f24bad4c5c38
TOUHAMI <amal.touhami@cgi.com>
2024-03-11 13:42:49 +0000

    entretien-technique model update

--- src/app/models/entretien-technique.model.ts
+++ src/app/models/entretien-technique.model.ts
@@ -1,2 +1,21 @@
+import { Avis} from "app/enums/avis";
+import { Statut} from "app/enums/statut";
+import { User} from "app/models/user";
+import { Candidat} from "app/models/candidat.model";
+
 export class EntretienTechnique {
+  id: number;
+  title: string;
+  date_entretien_tec: string;
+  heure_entretien_tec: string;
+  lien_reunion: string;
+  motivation: string;
+  commentaire: string;
+  conclusion_quest: string;
+  avis_tec: Avis;
+  justif_avis: string;
+  rapportTech: Blob;
+  statut_entretien: Statut;
+  candidat: Candidat;
+  evaluateurs: User[];
 }
