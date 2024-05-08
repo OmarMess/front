14cf7973ee137f3f33cb7fd7ff7e00a4ce7b28ac
BOUBKRIOUI <ayoub.boubkrioui@cgi.com>
2024-05-08 19:14:13 +0100

    User or invalid password exception catched
    Now authentication has enforced by catching backend exception in the
    front

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,19 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(mail: string, password: string): Observable<any> {
-    return this.http.post<any>(this.baseUrl, { mail, password });
+    //return this.http.post<any>(this.baseUrl, { mail, password });
+    return this.http.post<any>(this.baseUrl, { mail, password }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Unauthorized error
+          return throwError('Invalid email or password.');
+        } else {
+          // Other errors
+          return throwError('An error occurred while logging in. Please try again later.');
+        }
+      })
+    );
+  
   }
 
  
