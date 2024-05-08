081417957f399be7ebfc425e319f2005a8a7c9cd
BOUBKRIOUI <ayoub.boubkrioui@cgi.com>
2024-05-08 19:33:25 +0100

    Revert "User or invalid password exception catched"
    This reverts commit 14cf7973ee137f3f33cb7fd7ff7e00a4ce7b28ac.

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +11,7 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(mail: string, password: string): Observable<any> {
-    //return this.http.post<any>(this.baseUrl, { mail, password });
-    return this.http.post<any>(this.baseUrl, { mail, password }).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 401) {
-          // Unauthorized error
-          return throwError('Invalid email or password.');
-        } else {
-          // Other errors
-          return throwError('An error occurred while logging in. Please try again later.');
-        }
-      })
-    );
-  
+    return this.http.post<any>(this.baseUrl, { mail, password });
   }
 
  
