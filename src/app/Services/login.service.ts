f84f7f6bb5acce9f7c89b2d11b6c9b822098e980
BOUBKRIOUI <ayoub.boubkrioui@cgi.com>
2024-05-09 05:50:04 +0100

    trying to fix conflicts, There changes in login_f branch which dosn't exist in main

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -11,18 +11,18 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(mail: string, password: string): Observable<any> {
-    return this.http.post<any>(this.baseUrl, { mail, password });
-    // return this.http.post<any>(this.baseUrl, { mail, password }).pipe(
-    //   catchError((error: HttpErrorResponse) => {
-    //     if (error.status === 401) {
-    //       // Unauthorized error
-    //       return throwError('Invalid email or password.');
-    //     } else {
-    //       // Other errors
-    //       return throwError('An error occurred while logging in. Please try again later.');
-    //     }
-    //   })
-    // );
+    // return this.http.post<any>(this.baseUrl, { mail, password });
+    return this.http.post<any>(this.baseUrl, { mail, password }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Unauthorized error
+          return throwError('Invalid email or password.');
+        } else {
+          // Other errors
+          return throwError('An error occurred while logging in. Please try again later.');
+        }
+      })
+    );
   
   }
 
